00aa2f69004c67f85a29d21795e747aedef108b1
hassen rachdi <hassenrachdi20@gmail.com>
2024-02-13 16:53:02 +0100

    first commit: hw to connect a jdbc database

---
+++ src/main/java/com/example/demo/metier/Test.java
@@ -0,0 +1,26 @@
+package com.example.demo.metier;
+import java.sql.*;
+
+public class Test {
+    public static void main(String args[]) {
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "hass09090123rach";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection is Successful to the database" + url);
+            String query = "Insert into student(idstudent,namestud) values(101,'ram')";
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+}
+
